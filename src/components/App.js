4be62ce77193c37c6e6f32b24a4c635025a99034
Sharmina Akhtar <sakht318@gmail.com>
2018-10-23 10:28:39 +0000

    automatically backed up by learn

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,7 @@ class App extends Component {
 
     render() {
       return (
-        <div className="fruit-basket">
+        <div>
           <Filter />
           <FilteredFruitList />
           <FruitBasket />
